d786742a6be17dc441d57898fda7d165fcf2527c
ailatscmir <a.ilat@scmi.ru>
2018-01-24 17:43:37 +0300

    react-hammer turned on

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,61 @@
 import React, {Component} from 'react';
 import './App.css';
+import Hammer from "react-hammerjs";
+import svgmap from './floor1_20171114203824923.svg';
+import renderHTML from 'react-render-html';
+
+
 class App extends Component {
-    state = {
-        stores: []
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      stores: [],
+      hammerAction: 'no action',
+      storeselected: ''
+    };
 
-    componentDidMount() {
-        fetch('/stores').then(res => res.json()).then((stores) => {
-          this.setState({stores})
-        });
-    }
+    this.handleTap = this.handleTap.bind(this);
+    this.handleSwipe = this.handleSwipe.bind(this);
+    this.handlePan = this.handlePan.bind(this);
+  }
 
-    render() {
+  componentDidMount() {
+    fetch('/stores').then(res => res.json()).then((stores) => {
 
+      this.setState({stores})
+    });
+  }
 
-        return (<div className="App">
-            <h1>Stores</h1>
-            {this.state.stores.map(store =>
-              <div key={store.id}>{store.post_title}</div>
-            )}
-        </div>);
-    }
+  handleTap(ev) {
+    console.log(ev.target.id);
+  }
+
+  handleSwipe() {
+    this.setState({hammerAction: 'swipe'});
+    console.log('swipe');
+  }
+  handlePan(ev){
+    (ev.additionalEvent)? this.setState({hammerAction: ev.additionalEvent}) : this.setState({hammerAction: 'no direction'});
+    console.log(ev);
+  }
+
+  render() {
+    return (<div className="App">
+      <h1>Stores</h1>
+      <Hammer onTap={this.handleTap}
+              onSwipe={this.handleSwipe}
+              onPan={this.handlePan}>
+        <div>{renderHTML(svgmap)}</div>
+      </Hammer>
+
+      <p>{this.state.hammerAction}</p>
+      {
+        this.state.stores.map(store => <div key={store.ID}>{store.post_title}
+        </div>)
+      }
+
+    </div>);
+  }
 }
 
 export default App;
---
+++ frontend/src/ClickableSvg.js
@@ -0,0 +1,10 @@
+import {Component} from 'react';
+const svgmap = require('./floor1_20171114203824923.svg');
+
+class ClickableSvg extends Component {
+  render() {
+    return (svgmap);
+  }
+}
+
+export default ClickableSvg;
