a64ac5f274bc5239ae48ff139c98fce1cd2ad3af
ailatscmir <a.ilat@scmi.ru>
2018-01-30 15:23:19 +0300

    click works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
+import ClickableSvg from './ClickableSvg';
 // import Hammer from "react-hammerjs";
-// import svgmap from './floor1_20171114203824923.svg';
+
+
 // import renderHTML from 'react-render-html';
-import MyD3Comp from './MyD3Comp';
+
 
 class App extends Component {
   constructor(props) {
@@ -13,48 +15,25 @@ class App extends Component {
       hammerAction: 'no action',
       storeselected: ''
     };
-
-    this.handleTap = this.handleTap.bind(this);
-    this.handleSwipe = this.handleSwipe.bind(this);
-    this.handlePan = this.handlePan.bind(this);
   }
 
   componentDidMount() {
     // console.log(svgmap);
     fetch('/stores').then(res => res.json()).then((stores) => {
-      this.setState({stores})
+      // this.setState({stores})
     });
-  }
 
-  handleTap(ev) {
-    this.setState({hammerAction: 'clicked '+ev.target.id})
-    console.log(ev.target.id);
-  }
-
-  handleSwipe() {
-    this.setState({hammerAction: 'swipe'});
-    console.log('swipe');
-  }
-  handlePan(ev){
-    (ev.additionalEvent)? this.setState({hammerAction: ev.additionalEvent}) : this.setState({hammerAction: 'no direction'});
-    console.log(ev);
   }
 
   render() {
-    return (<div className="App">
-      <h1>Stores</h1>
-      <MyD3Comp />
+
+    return (<div id="App">
+      <ClickableSvg />
       {/* <Hammer onTap={this.handleTap}
               onSwipe={this.handleSwipe}
               onPan={this.handlePan}>
         <div>{renderHTML(svgmap)}</div>
       </Hammer> */}
-
-      <p>{this.state.hammerAction}</p>
-      {
-        // this.state.stores.map(store => <div key={store.ID}>{store.post_title}</div>)
-      }
-
     </div>);
   }
 }
---
+++ frontend/src/Category.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+const categoryColors = {
+  clothes: '#F2C94C',
+  entertaiment: '#BB6BD9',
+  maintance: '#56CCF2',
+  foodandhome: '#EB5757',
+  electronics: '#2D9CDB'
+};
+class Category extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      paths: props.children.path
+    };
+    this.storePathSelect = this.storePathSelect.bind(this);
+  }
+
+  storePathSelect(ev) {
+    console.log(ev.target);
+    return true;
+  }
+
+  render() {
+    var paths = this.state.paths;
+    return (Object.values(paths).map((path) => {
+      // return <text key={index} y={(index+3)*30}>{value.id}</text>;
+      return <path id={path.id} key={path.id} d={path.d} onClick={this.storePathSelect}></path>;
+    }))
+  }
+}
+export default Category;
